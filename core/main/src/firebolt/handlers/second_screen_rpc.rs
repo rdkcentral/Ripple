1fd901fe321e1734be366e846ef0117a1ed52007
Fasil K V <fasil.k@tataelxsi.co.in>
2023-05-02 10:30:50 +0530

    feat: api's added.

--- core/main/src/firebolt/handlers/second_screen_rpc.rs
+++ core/main/src/firebolt/handlers/second_screen_rpc.rs
@@ -1,35 +1,26 @@
 use std::collections::HashMap;
 
-use jsonrpsee::core::async_trait;
-use jsonrpsee::core::RpcResult;
-use jsonrpsee::proc_macros::rpc;
-use jsonrpsee::RpcModule;
-use tokio::sync::oneshot;
-use tracing::instrument;
-
-use crate::api::rpc::rpc_gateway::RPCProvider;
-use crate::apps::app_events::AppEvents;
-use crate::apps::app_events::ListenRequest;
-use crate::apps::app_events::ListenerResponse;
-use crate::apps::app_mgr::{AppError, AppManagerResponse, AppMethod, AppRequest};
-use crate::helpers::error_util::rpc_await_oneshot;
-use crate::helpers::ripple_helper::IRippleHelper;
-use crate::helpers::ripple_helper::RippleHelperFactory;
-use crate::helpers::ripple_helper::RippleHelperType;
-use crate::managers::capability_manager::CapClassifiedRequest;
-use crate::managers::capability_manager::FireboltCap;
-use crate::managers::capability_manager::IGetLoadedCaps;
-use crate::managers::capability_manager::RippleHandlerCaps;
-use crate::platform_state::PlatformState;
-use crate::{api::rpc::rpc_gateway::CallContext, helpers::ripple_helper::RippleHelper};
+use crate::{
+    firebolt::rpc::RippleRPCProvider, state::platform_state::PlatformState,
+    utils::rpc_utils::rpc_add_event_listener,
+};
+use jsonrpsee::{
+    core::{async_trait, RpcResult},
+    proc_macros::rpc,
+    RpcModule,
+};
+use ripple_sdk::api::{
+    firebolt::fb_general::{ListenRequest, ListenerResponse},
+    gateway::rpc_gateway_api::CallContext,
+};
 use serde::{Deserialize, Serialize};
 
-use super::device::get_device_id;
-use super::device::get_device_name;
-use super::discovery::LaunchRequest;
+use super::device_rpc::{get_device_id, get_device_name};
 
 pub const EVENT_SECOND_SCREEN_ON_LAUNCH_REQUEST: &'static str = "secondscreen.onLaunchRequest";
 pub const EVENT_SECOND_SCREEN_ON_CLOSE_REQUEST: &'static str = "secondscreen.onCloseRequest";
+pub const EVENT_SECOND_SCREEN_ON_FRIENDLY_NAME_CHANGED: &'static str =
+    "secondscreen.onFriendlyNameChanged";
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SecondScreenDeviceInfo {
@@ -65,41 +56,18 @@ pub trait SecondScreen {
     ) -> RpcResult<ListenerResponse>;
 }
 
-pub struct SecondScreenImpl<IRippleHelper> {
-    pub helper: Box<IRippleHelper>,
-    pub platform_state: PlatformState,
-}
-
-impl SecondScreenImpl<RippleHelper> {
-    async fn listen(
-        &self,
-        ctx: CallContext,
-        request: ListenRequest,
-        event_name: &'static str,
-    ) -> RpcResult<ListenerResponse> {
-        let listen = request.listen;
-
-        AppEvents::add_listener(
-            &&self.platform_state.app_events_state,
-            event_name.to_string(),
-            ctx,
-            request,
-        );
-        Ok(ListenerResponse {
-            listening: listen,
-            event: event_name,
-        })
-    }
+pub struct SecondScreenImpl {
+    pub state: PlatformState,
 }
 
 #[async_trait]
-impl SecondScreenServer for SecondScreenImpl<RippleHelper> {
+impl SecondScreenServer for SecondScreenImpl {
     async fn device(&self, ctx: CallContext, param: SecondScreenDeviceInfo) -> RpcResult<String> {
-        get_device_id(&self.helper, ctx).await
+        get_device_id(&self.state).await
     }
 
     async fn friendly_name(&self, _ctx: CallContext) -> RpcResult<String> {
-        get_device_name(&self.platform_state).await
+        get_device_name(&self.state).await
     }
 
     async fn protocols(&self, _ctx: CallContext) -> RpcResult<HashMap<String, bool>> {
@@ -114,8 +82,13 @@ impl SecondScreenServer for SecondScreenImpl<RippleHelper> {
         ctx: CallContext,
         request: ListenRequest,
     ) -> RpcResult<ListenerResponse> {
-        self.listen(ctx, request, EVENT_SECOND_SCREEN_ON_LAUNCH_REQUEST)
-            .await
+        rpc_add_event_listener(
+            &self.state,
+            ctx,
+            request,
+            EVENT_SECOND_SCREEN_ON_LAUNCH_REQUEST,
+        )
+        .await
     }
 
     async fn on_close_request(
@@ -123,70 +96,33 @@ impl SecondScreenServer for SecondScreenImpl<RippleHelper> {
         ctx: CallContext,
         request: ListenRequest,
     ) -> RpcResult<ListenerResponse> {
-        self.listen(ctx, request, EVENT_SECOND_SCREEN_ON_CLOSE_REQUEST)
-            .await
+        rpc_add_event_listener(
+            &self.state,
+            ctx,
+            request,
+            EVENT_SECOND_SCREEN_ON_CLOSE_REQUEST,
+        )
+        .await
     }
 
-    #[instrument(skip(self))]
     async fn on_friendly_name_changed(
         &self,
         ctx: CallContext,
         request: ListenRequest,
     ) -> RpcResult<ListenerResponse> {
-        let listen = request.listen;
-
-        AppEvents::add_listener(
-            &&self.platform_state.app_events_state,
-            "secondscreen.onFriendlyNameChanged".to_string(),
+        rpc_add_event_listener(
+            &self.state,
             ctx,
             request,
-        );
-        Ok(ListenerResponse {
-            listening: listen,
-            event: "secondscreen.onFriendlyNameChanged",
-        })
-    }
-
-}
-
-pub struct SecondScreenRippleProvider;
-
-pub struct SecondScreenCapHandler;
-
-impl IGetLoadedCaps for SecondScreenCapHandler {
-    fn get_loaded_caps(&self) -> RippleHandlerCaps {
-        RippleHandlerCaps {
-            caps: Some(vec![CapClassifiedRequest::Supported(vec![
-                FireboltCap::Short("protocol:dial".into()),
-            ])]),
-        }
+            EVENT_SECOND_SCREEN_ON_FRIENDLY_NAME_CHANGED,
+        )
+        .await
     }
 }
 
-impl RPCProvider<SecondScreenImpl<RippleHelper>, SecondScreenCapHandler>
-    for SecondScreenRippleProvider
-{
-    fn provide(
-        self,
-        rhf: Box<RippleHelperFactory>,
-        platform_state: PlatformState,
-    ) -> (
-        RpcModule<SecondScreenImpl<RippleHelper>>,
-        SecondScreenCapHandler,
-    ) {
-        let ss = SecondScreenImpl {
-            helper: rhf.clone().get(self.get_helper_variant()),
-            platform_state,
-        };
-        (ss.into_rpc(), SecondScreenCapHandler)
-    }
-
-    fn get_helper_variant(self) -> Vec<RippleHelperType> {
-        vec![
-            RippleHelperType::AppManager,
-            RippleHelperType::Dab,
-            RippleHelperType::Dpab,
-            RippleHelperType::Cap,
-        ]
+pub struct SecondScreenRPCProvider;
+impl RippleRPCProvider<SecondScreenImpl> for SecondScreenRPCProvider {
+    fn provide(state: PlatformState) -> RpcModule<SecondScreenImpl> {
+        (SecondScreenImpl { state }).into_rpc()
     }
 }
