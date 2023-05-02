a3e175af1326e1707c102d84c1a65cb201174ced
Sathishkumar <satlead@gmail.com>
2023-05-02 09:55:07 -0400

    feat:  Porting second screen firebolt api base fixes #18 (#80)
    * base file added.
    
    * files renamed.
    
    * feat: porting second-screen firebolt api fixes #18 (#79)
    
    Co-authored-by: Fasil K V <fasil.k@tataelxsi.co.in>
    
    * fix: warnings
    
    * fix: add header
    
    ---------
    
    Co-authored-by: Fasil K V <fasil.k@tataelxsi.co.in>
    Co-authored-by: kvfasil <82154972+kvfasil@users.noreply.github.com>

--- core/main/src/bootstrap/start_fbgateway_step.rs
+++ core/main/src/bootstrap/start_fbgateway_step.rs
@@ -24,8 +24,8 @@ use crate::{
             device_rpc::DeviceRPCProvider, keyboard_rpc::KeyboardRPCProvider,
             lcm_rpc::LifecycleManagementProvider, lifecycle_rpc::LifecycleRippleProvider,
             localization_rpc::LocalizationRPCProvider, pin_rpc::PinRPCProvider,
-            profile_rpc::ProfileRPCProvider, voice_guidance_rpc::VoiceguidanceRPCProvider,
-            wifi_rpc::WifiRPCProvider,
+            profile_rpc::ProfileRPCProvider, second_screen_rpc::SecondScreenRPCProvider,
+            voice_guidance_rpc::VoiceguidanceRPCProvider, wifi_rpc::WifiRPCProvider,
         },
         rpc::RippleRPCProvider,
     },
@@ -52,6 +52,8 @@ impl FireboltGatewayStep {
         let _ = methods.merge(LocalizationRPCProvider::provide_with_alias(state.clone()));
         let _ = methods.merge(AccessoryRippleProvider::provide_with_alias(state.clone()));
         let _ = methods.merge(ProfileRPCProvider::provide(state.clone()));
+        let _ = methods.merge(SecondScreenRPCProvider::provide_with_alias(state.clone()));
+
         // LCM Api(s) not required for internal launcher
         if !state.has_internal_launcher() {
             let _ = methods.merge(LifecycleManagementProvider::provide_with_alias(
---
+++ core/main/src/firebolt/handlers/second_screen_rpc.rs
@@ -0,0 +1,144 @@
+// If not stated otherwise in this file or this component's license file the
+// following copyright and licenses apply:
+//
+// Copyright 2023 RDK Management
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use std::collections::HashMap;
+
+use crate::{
+    firebolt::rpc::RippleRPCProvider, state::platform_state::PlatformState,
+    utils::rpc_utils::rpc_add_event_listener,
+};
+use jsonrpsee::{
+    core::{async_trait, RpcResult},
+    proc_macros::rpc,
+    RpcModule,
+};
+use ripple_sdk::api::{
+    firebolt::fb_general::{ListenRequest, ListenerResponse},
+    gateway::rpc_gateway_api::CallContext,
+};
+use serde::{Deserialize, Serialize};
+
+use super::device_rpc::{get_device_id, get_device_name};
+
+pub const EVENT_SECOND_SCREEN_ON_LAUNCH_REQUEST: &'static str = "secondscreen.onLaunchRequest";
+pub const EVENT_SECOND_SCREEN_ON_CLOSE_REQUEST: &'static str = "secondscreen.onCloseRequest";
+pub const EVENT_SECOND_SCREEN_ON_FRIENDLY_NAME_CHANGED: &'static str =
+    "secondscreen.onFriendlyNameChanged";
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SecondScreenDeviceInfo {
+    #[serde(rename = "type")]
+    pub _type: Option<String>,
+}
+
+#[rpc(server)]
+pub trait SecondScreen {
+    #[method(name = "secondscreen.device")]
+    async fn device(&self, ctx: CallContext, param: SecondScreenDeviceInfo) -> RpcResult<String>;
+    #[method(name = "secondscreen.friendlyName")]
+    async fn friendly_name(&self, ctx: CallContext) -> RpcResult<String>;
+    #[method(name = "secondscreen.protocols")]
+    async fn protocols(&self, _ctx: CallContext) -> RpcResult<HashMap<String, bool>>;
+    #[method(name = "secondscreen.onLaunchRequest")]
+    async fn on_launch_request(
+        &self,
+        ctx: CallContext,
+        request: ListenRequest,
+    ) -> RpcResult<ListenerResponse>;
+    #[method(name = "secondscreen.onCloseRequest")]
+    async fn on_close_request(
+        &self,
+        ctx: CallContext,
+        request: ListenRequest,
+    ) -> RpcResult<ListenerResponse>;
+    #[method(name = "secondscreen.onFriendlyNameChanged")]
+    async fn on_friendly_name_changed(
+        &self,
+        ctx: CallContext,
+        request: ListenRequest,
+    ) -> RpcResult<ListenerResponse>;
+}
+
+pub struct SecondScreenImpl {
+    pub state: PlatformState,
+}
+
+#[async_trait]
+impl SecondScreenServer for SecondScreenImpl {
+    async fn device(&self, _ctx: CallContext, _param: SecondScreenDeviceInfo) -> RpcResult<String> {
+        get_device_id(&self.state).await
+    }
+
+    async fn friendly_name(&self, _ctx: CallContext) -> RpcResult<String> {
+        get_device_name(&self.state).await
+    }
+
+    async fn protocols(&self, _ctx: CallContext) -> RpcResult<HashMap<String, bool>> {
+        let mut protocols = HashMap::<String, bool>::new();
+        // TODO: This is what we do for XClass, but should this really be defined in the device manifest or queried via DAB?
+        protocols.insert("dial2".into(), true);
+        Ok(protocols)
+    }
+
+    async fn on_launch_request(
+        &self,
+        ctx: CallContext,
+        request: ListenRequest,
+    ) -> RpcResult<ListenerResponse> {
+        rpc_add_event_listener(
+            &self.state,
+            ctx,
+            request,
+            EVENT_SECOND_SCREEN_ON_LAUNCH_REQUEST,
+        )
+        .await
+    }
+
+    async fn on_close_request(
+        &self,
+        ctx: CallContext,
+        request: ListenRequest,
+    ) -> RpcResult<ListenerResponse> {
+        rpc_add_event_listener(
+            &self.state,
+            ctx,
+            request,
+            EVENT_SECOND_SCREEN_ON_CLOSE_REQUEST,
+        )
+        .await
+    }
+
+    async fn on_friendly_name_changed(
+        &self,
+        ctx: CallContext,
+        request: ListenRequest,
+    ) -> RpcResult<ListenerResponse> {
+        rpc_add_event_listener(
+            &self.state,
+            ctx,
+            request,
+            EVENT_SECOND_SCREEN_ON_FRIENDLY_NAME_CHANGED,
+        )
+        .await
+    }
+}
+
+pub struct SecondScreenRPCProvider;
+impl RippleRPCProvider<SecondScreenImpl> for SecondScreenRPCProvider {
+    fn provide(state: PlatformState) -> RpcModule<SecondScreenImpl> {
+        (SecondScreenImpl { state }).into_rpc()
+    }
+}
