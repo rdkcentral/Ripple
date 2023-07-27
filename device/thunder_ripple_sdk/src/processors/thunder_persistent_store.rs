5599a3e8d5d60ccc8e0e3b5af996cc50e485a4bc
Sathishkumar Deena Kirupakaran <satlead@gmail.com>
2023-07-27 17:40:45 -0400

    fix: format

--- device/thunder_ripple_sdk/src/processors/thunder_persistent_store.rs
+++ device/thunder_ripple_sdk/src/processors/thunder_persistent_store.rs
@@ -20,9 +20,7 @@ use crate::{
     ripple_sdk::{
         api::device::{
             device_operator::{DeviceCallRequest, DeviceChannelParams, DeviceOperator},
-            device_peristence::{
-                DevicePersistenceRequest, GetStorageProperty, SetStorageProperty, StorageData,
-            },
+            device_peristence::{DevicePersistenceRequest, GetStorageProperty, SetStorageProperty},
         },
         async_trait::async_trait,
         extn::{
@@ -196,7 +194,7 @@ impl ThunderStorageRequestProcessor {
                 }
 
                 let value = parsed_res.unwrap();
-                if let Ok(v) =  serde_json::from_value(value.clone()) {
+                if let Ok(v) = serde_json::from_value(value.clone()) {
                     return Self::respond(
                         state.get_client(),
                         req.clone(),
