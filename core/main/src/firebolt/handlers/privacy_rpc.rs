787a5c77ac332f14a217d37dade3b9b15ba0ed36
Maggie Choy <maggie98choy@yahoo.com>
2023-09-07 10:21:19 -0700

    feat: fix formatting error

--- core/main/src/firebolt/handlers/privacy_rpc.rs
+++ core/main/src/firebolt/handlers/privacy_rpc.rs
@@ -521,18 +521,22 @@ impl PrivacyImpl {
             PrivacySettingsStorageType::Local => {
                 let payload = PrivacySettingsStoreRequest::SetPrivacySettings(property, value);
                 let response = platform_state.get_client().send_extn_request(payload).await;
-                if let Ok(extn_msg) =  response {
+                if let Ok(extn_msg) = response {
                     match extn_msg.payload {
-                        ExtnPayload::Response(res) => {
-                            match res {
-                                ExtnResponse::None(_) => RpcResult::Ok(()),
-                                _ => RpcResult::Err(jsonrpsee::core::Error::Custom("Unable to fetch".to_owned())),
-                            }
-                        }
-                        _ => RpcResult::Err(jsonrpsee::core::Error::Custom("Unexpected response received from Extn".to_owned()))
+                        ExtnPayload::Response(res) => match res {
+                            ExtnResponse::None(_) => RpcResult::Ok(()),
+                            _ => RpcResult::Err(jsonrpsee::core::Error::Custom(
+                                "Unable to fetch".to_owned(),
+                            )),
+                        },
+                        _ => RpcResult::Err(jsonrpsee::core::Error::Custom(
+                            "Unexpected response received from Extn".to_owned(),
+                        )),
                     }
                 } else {
-                    RpcResult::Err(jsonrpsee::core::Error::Custom("Error in getting response from Extn".to_owned()))
+                    RpcResult::Err(jsonrpsee::core::Error::Custom(
+                        "Error in getting response from Extn".to_owned(),
+                    ))
                 }
             }
             PrivacySettingsStorageType::Cloud => {
