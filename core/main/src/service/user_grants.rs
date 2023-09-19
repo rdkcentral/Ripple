9ad05c763589ae1e888f29cc6fd07a4054e0299e
Maggie Choy <maggie98choy@yahoo.com>
2023-09-19 15:23:50 -0700

    feat: update code

--- core/main/src/service/user_grants.rs
+++ core/main/src/service/user_grants.rs
@@ -1089,7 +1089,7 @@ impl GrantStepExecutor {
                 Err(_) => {
                     error!("Unable to get app state");
                     return Err(DenyReasonWithCap {
-                        reason: DenyReason::AppNotInActiveState,
+                        reason: DenyReason::Unavailable,
                         caps: vec![permission.cap.clone()],
                     });
                 }
