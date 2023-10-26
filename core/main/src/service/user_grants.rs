e677dd59bda5e8cdab905edeea6ede7d3f5a2338
Vinod Sathyaseelan <vinod_sathyaseelan@comcast.com>
2023-10-25 18:30:08 -0700

    fix: Fixed issues with SessionTransitionCanceled event notification.

--- core/main/src/service/user_grants.rs
+++ core/main/src/service/user_grants.rs
@@ -1024,7 +1024,7 @@ impl GrantPolicyEnforcer {
                 )
                 .await
                 {
-                    debug!("grant step execute Err. step={:?}", step);
+                    debug!("grant step execute Err. step={:?} e={:?}", step, e);
                     CapState::emit(
                         platform_state,
                         CapEvent::OnRevoked,
