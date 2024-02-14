a59df186f4c0a987899b92ce6ddc87ef8a399e14
pahearn73 <Peter_Ahearn@cable.comcast.com>
2024-02-14 18:50:55 -0500

    Metrics Observability: WIP

--- core/main/src/service/observability/mod.rs
+++ core/main/src/service/observability/mod.rs
@@ -5,23 +5,7 @@ use ripple_sdk::api::firebolt::fb_telemetry::OperationalMetricRequest;
 static mut PLATFORM_STATE: Option<Arc<PlatformState>> = None;
 pub struct ObservabilityClient {}
 impl ObservabilityClient {
-    //     pub fn init(ps: PlatformState) {
-    //         unsafe {
-    //             PLATFORM_STATE = Some(Arc::new(ps));
-    //         }
-    //     }
     pub fn report(platform_state: &PlatformState, payload: OperationalMetricRequest) {
-        //  match unsafe { PLATFORM_STATE } {
-        // Some(ref ps) => {
-        //     let request = ripple_sdk::api::observability::ObservabilityRequest {
-        //         payload: payload.,
-        //     };
-        //     let _ = ps.get_client().send_extn_request_transient(request);
-        // },
-        // None => {
-        //     println!("ObservabilityClient not initialized");
-        // }
-        //}
         println!("payload: {:?}", payload);
     }
 }
