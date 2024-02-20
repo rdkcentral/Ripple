f1ff41becb2312e3a4f590d5d091ce9b5c2ec158
pahearn73 <Peter_Ahearn@cable.comcast.com>
2024-02-20 12:03:32 -0500

    Metrics Observability: Fixed service interaction request.

--- core/sdk/src/api/observability/metrics_util.rs
+++ core/sdk/src/api/observability/metrics_util.rs
@@ -1,9 +1,11 @@
 use log::{debug, error};
 
 use crate::{
-    api::firebolt::fb_metrics::{
-        get_metrics_tags, InteractionType, MetricsPayload, MetricsRequest,
-        OperationalMetricPayload, Tag, Timer, SERVICE_METRICS_SEND_REQUEST_TIMEOUT_MS,
+    api::firebolt::{
+        fb_metrics::{
+            get_metrics_tags, InteractionType, Tag, Timer, SERVICE_METRICS_SEND_REQUEST_TIMEOUT_MS,
+        },
+        fb_telemetry::OperationalMetricRequest,
     },
     extn::{client::extn_client::ExtnClient, extn_client_message::ExtnResponse},
     utils::error::RippleError,
@@ -34,10 +36,7 @@ pub async fn stop_and_send_service_metrics_timer(
 
         debug!("stop_and_send_service_metrics_timer: {:?}", timer);
 
-        let req = MetricsRequest {
-            payload: MetricsPayload::OperationalMetric(OperationalMetricPayload::Timer(timer)),
-            context: None,
-        };
+        let req = OperationalMetricRequest::Timer(timer);
 
         let resp: Result<ExtnResponse, RippleError> = client
             .standalone_request(req, SERVICE_METRICS_SEND_REQUEST_TIMEOUT_MS)
