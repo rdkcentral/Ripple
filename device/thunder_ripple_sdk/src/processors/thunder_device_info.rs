7ac5fc1f4eeb702439072b0176caef16c0667a74
pahearn73 <pahearn73@users.noreply.github.com>
2024-03-14 15:14:42 -0400

    Metrics: Fields: Added support for new metrics fields (#412)

--- device/thunder_ripple_sdk/src/processors/thunder_device_info.rs
+++ device/thunder_ripple_sdk/src/processors/thunder_device_info.rs
@@ -64,7 +64,7 @@ use ripple_sdk::{
         config::Config,
         context::RippleContextUpdateRequest,
         device::{
-            device_info_request::PlatformBuildInfo,
+            device_info_request::{FirmwareInfo, PlatformBuildInfo},
             device_request::{InternetConnectionStatus, PowerState},
         },
         device::{
@@ -981,11 +981,11 @@ impl ThunderDeviceInfoRequestProcessor {
         }
     }
 
-    async fn get_version(state: &CachedState) -> FireboltSemanticVersion {
-        let response: FireboltSemanticVersion;
+    async fn get_os_info(state: &CachedState) -> FirmwareInfo {
+        let version: FireboltSemanticVersion;
         // TODO: refactor this to use return syntax and not use response variable across branches
         match state.get_version() {
-            Some(v) => response = v,
+            Some(v) => version = v,
             None => {
                 let resp = state
                     .get_thunder_client()
@@ -1004,27 +1004,30 @@ impl ThunderDeviceInfoRequestProcessor {
                     let minor: String = tsv_vec[1].chars().filter(|c| c.is_ascii_digit()).collect();
                     let patch: String = tsv_vec[2].chars().filter(|c| c.is_ascii_digit()).collect();
 
-                    response = FireboltSemanticVersion {
+                    version = FireboltSemanticVersion {
                         major: major.parse::<u32>().unwrap(),
                         minor: minor.parse::<u32>().unwrap(),
                         patch: patch.parse::<u32>().unwrap(),
                         readable: tsv.stb_version,
                     };
-                    state.update_version(response.clone());
+                    state.update_version(version.clone());
                 } else {
-                    response = FireboltSemanticVersion {
+                    version = FireboltSemanticVersion {
                         readable: tsv.stb_version,
                         ..FireboltSemanticVersion::default()
                     };
-                    state.update_version(response.clone())
+                    state.update_version(version.clone())
                 }
             }
         }
-        response
+        FirmwareInfo {
+            name: "rdk".into(),
+            version,
+        }
     }
 
-    async fn version(state: CachedState, req: ExtnMessage) -> bool {
-        let response = Self::get_version(&state).await;
+    async fn os_info(state: CachedState, req: ExtnMessage) -> bool {
+        let response = Self::get_os_info(&state).await;
         Self::respond(
             state.get_client(),
             req,
@@ -1381,7 +1384,7 @@ impl ThunderDeviceInfoRequestProcessor {
             },
             async {
                 if device_info_authorized {
-                    Some(Self::get_version(&state).await)
+                    Some(Self::get_os_info(&state).await.version)
                 } else {
                     None
                 }
@@ -1595,7 +1598,7 @@ impl ExtnRequestProcessor for ThunderDeviceInfoRequestProcessor {
             DeviceInfoRequest::VideoResolution => Self::video_resolution(state.clone(), msg).await,
             DeviceInfoRequest::Network => Self::network(state.clone(), msg).await,
             DeviceInfoRequest::Make => Self::make(state.clone(), msg).await,
-            DeviceInfoRequest::Version => Self::version(state.clone(), msg).await,
+            DeviceInfoRequest::FirmwareInfo => Self::os_info(state.clone(), msg).await,
             DeviceInfoRequest::AvailableMemory => Self::available_memory(state.clone(), msg).await,
             DeviceInfoRequest::OnInternetConnected(time_out) => {
                 Self::on_internet_connected(state.clone(), msg, time_out.timeout).await
