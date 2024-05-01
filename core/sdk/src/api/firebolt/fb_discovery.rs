282a6dc586f17b86d10dd81c261dc002a2016254
Suresh Kumar <suresh_kumar3@comcast.com>
2024-05-01 11:29:15 -0700

    build: cleanup

--- core/sdk/src/api/firebolt/fb_discovery.rs
+++ core/sdk/src/api/firebolt/fb_discovery.rs
@@ -291,13 +291,13 @@ pub struct SignInRequestParams {
     pub is_signed_in: bool, /*true for signIn, false for signOut */
 }
 
-#[derive(Debug, Clone)]
-pub enum DiscoveryAccountLinkRequest {
-    EntitlementsAccountLink(EntitlementsAccountLinkRequestParams),
-    MediaEventAccountLink(MediaEventsAccountLinkRequestParams),
-    LaunchPadAccountLink(LaunchPadAccountLinkRequestParams),
-    SignIn(SignInRequestParams),
-}
+//#[derive(Debug, Clone)]
+//pub enum DiscoveryAccountLinkRequest {
+//    EntitlementsAccountLink(EntitlementsAccountLinkRequestParams),
+//    MediaEventAccountLink(MediaEventsAccountLinkRequestParams),
+//    LaunchPadAccountLink(LaunchPadAccountLinkRequestParams),
+//    SignIn(SignInRequestParams),
+//}
 
 pub const PROGRESS_UNIT_SECONDS: &str = "Seconds";
 pub const PROGRESS_UNIT_PERCENT: &str = "Percent";
