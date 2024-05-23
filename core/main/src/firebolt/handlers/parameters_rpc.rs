08adab768a6c71ebb3f68e46684d65fa4cf55897
Brindha Senthil <brindha_senthil@cable.comcast.com>
2024-05-23 08:20:21 -0700

    fix: remove us-privacy for non us devices

--- core/main/src/firebolt/handlers/parameters_rpc.rs
+++ core/main/src/firebolt/handlers/parameters_rpc.rs
@@ -25,15 +25,14 @@ use ripple_sdk::{
         apps::{AppManagerResponse, AppMethod, AppRequest, AppResponse},
         device::entertainment_data::NavigationIntent,
         gateway::rpc_gateway_api::CallContext,
-        storage_property::StorageProperty,
     },
     tokio::sync::oneshot,
 };
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    firebolt::rpc::RippleRPCProvider, processor::storage::storage_manager::StorageManager,
-    state::platform_state::PlatformState, utils::rpc_utils::rpc_await_oneshot,
+    firebolt::rpc::RippleRPCProvider, state::platform_state::PlatformState,
+    utils::rpc_utils::rpc_await_oneshot,
 };
 
 use super::privacy_rpc;
@@ -95,19 +94,8 @@ impl ParametersServer for ParametersImpl {
             .send_app_request(app_request);
         let resp = rpc_await_oneshot(app_resp_rx).await?;
         if let AppManagerResponse::LaunchRequest(launch_req) = resp? {
-            let country_code =
-                StorageManager::get_string(&self.platform_state, StorageProperty::CountryCode)
-                    .await
-                    .unwrap_or_default();
-
-            let us_privacy = if country_code == "US" {
-                privacy_data.get(privacy_rpc::US_PRIVACY_KEY).cloned()
-            } else {
-                None
-            };
-
             return Ok(AppInitParameters {
-                us_privacy,
+                us_privacy: privacy_data.get(privacy_rpc::US_PRIVACY_KEY).cloned(),
                 lmt: privacy_data
                     .get(privacy_rpc::LMT_KEY)
                     .and_then(|x| x.parse::<u16>().ok()),
