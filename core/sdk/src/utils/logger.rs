e566533f049a9d03d3349a5b8690dcb56837254f
nnaveen979 <naveen.dunna057@gmail.com>
2024-07-24 13:42:35 +0530

    docs:made the alignment correct to avoid the format check failure

--- core/sdk/src/utils/logger.rs
+++ core/sdk/src/utils/logger.rs
@@ -44,7 +44,7 @@ pub fn init_logger(name: String) -> Result<(), fern::InitError> {
             ));
         })
         .level(filter)
-	//log filter applied here, making the log level to OFF for the below mentioned crates
+        //log filter applied here, making the log level to OFF for the below mentioned crates
         .level_for("h2", log::LevelFilter::Off)
         .level_for("hyper", log::LevelFilter::Off)
         .level_for("rustls", log::LevelFilter::Off)
@@ -109,7 +109,7 @@ pub fn init_and_configure_logger(version: &str, name: String) -> Result<(), fern
             }
         })
         .level(filter)
-	//log filter applied here, making the log level to OFF for the below mentioned crates
+        //log filter applied here, making the log level to OFF for the below mentioned crates
         .level_for("h2", log::LevelFilter::Off)
         .level_for("hyper", log::LevelFilter::Off)
         .level_for("rustls", log::LevelFilter::Off)
