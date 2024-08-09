61ea1c534044cb9ccf93a9343ed80d9238d4074d
maggie98choy <maggie98choy@yahoo.com>
2024-08-08 18:00:12 -0700

    add more unit test

--- core/main/src/broker/endpoint_broker.rs
+++ core/main/src/broker/endpoint_broker.rs
@@ -894,7 +894,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_apply_response_do_not_contains_error() {
+    async fn test_apply_response_contains_result() {
         let result = json!({"stbVersion":"SCXI11BEI_VBN_24Q3_sprint_20240717150752sdy_FG","receiverVersion":"7.6.0.0","stbTimestamp":"Wed 17 Jul 2024 15:07:52 UTC","success":true});
         let ctx = CallContext::new(
             "session_id".to_string(),
@@ -914,5 +914,67 @@ mod tests {
         response.result = Some(result);
         apply_response(response, filter, &rpc_request, &mut output);
         assert_eq!(output.data.result.unwrap(), "SCXI11BEI".to_string());
+
+        let result = json!("Resolution1080P");
+        let ctx = CallContext::new(
+            "session_id".to_string(),
+            "request_id".to_string(),
+            "app_id".to_string(),
+            1,
+            ApiProtocol::Bridge,
+            "method".to_string(),
+            Some("cid".to_string()),
+            true,
+        );
+        let rpc_request = RpcRequest::new("new_method".to_string(), "params".to_string(), ctx);
+        let data = JsonRpcApiResponse::mock();
+        let mut output: BrokerOutput = BrokerOutput { data: data.clone() };
+        let filter = "if .result then if .result | contains(\"480\") then ( [640, 480] ) elif .result | contains(\"576\") then ( [720, 576] ) elif .result | contains(\"1080\") then ( [1920, 1080] ) elif .result | contains(\"2160\") then ( [2160, 1440] ) end elif .error then if .error.code == -32601 then \"Unknown method.\" else \"Error occurred with a different code\" end else \"No result or recognizable error\" end".to_string();
+        let mut response = JsonRpcApiResponse::mock();
+        response.result = Some(result);
+        apply_response(response, filter, &rpc_request, &mut output);
+        assert_eq!(output.data.result.unwrap(), json!([1920, 1080]));
+
+        let result = json!({"currentAudioFormat":"DOLBY AC3","supportedAudioFormat":["NONE","PCM","AAC","VORBIS","WMA","DOLBY AC3","DOLBY AC4","DOLBY MAT","DOLBY TRUEHD","DOLBY EAC3 ATMOS","DOLBY TRUEHD ATMOS","DOLBY MAT ATMOS","DOLBY AC4 ATMOS","UNKNOWN"],"success":true});
+        let ctx = CallContext::new(
+            "session_id".to_string(),
+            "request_id".to_string(),
+            "app_id".to_string(),
+            1,
+            ApiProtocol::Bridge,
+            "method".to_string(),
+            Some("cid".to_string()),
+            true,
+        );
+        let rpc_request = RpcRequest::new("new_method".to_string(), "params".to_string(), ctx);
+        let data = JsonRpcApiResponse::mock();
+        let mut output: BrokerOutput = BrokerOutput { data: data.clone() };
+        let filter = "if .result and .result.success then .result | {\"stereo\": (.supportedAudioFormat |  index(\"PCM\") > 0),\"dolbyDigital5.1\": (.supportedAudioFormat |  index(\"DOLBY AC3\") > 0),\"dolbyDigital5.1plus\": (.supportedAudioFormat |  index(\"DOLBY EAC3\") > 0),\"dolbyAtmos\": (.supportedAudioFormat |  index(\"DOLBY EAC3 ATMOS\") > 0)} elif .error then if .error.code == -32601 then \"Unknown method.\" else \"Error occurred with a different code\" end else \"No result or recognizable error\" end".to_string();
+        let mut response = JsonRpcApiResponse::mock();
+        response.result = Some(result);
+        apply_response(response, filter, &rpc_request, &mut output);
+        assert_eq!(output.data.result.unwrap(), json!({"dolbyAtmos": true, "dolbyDigital5.1": true, "dolbyDigital5.1plus": false, "stereo": true}));
+
+        let result = json!({"interfaces":[{"interface":"ETHERNET","macAddress":
+        "f0:46:3b:5b:eb:14","enabled":true,"connected":false},{"interface":"WIFI","macAddress
+        ":"f0:46:3b:5b:eb:15","enabled":true,"connected":true}],"success":true});
+        let ctx = CallContext::new(
+            "session_id".to_string(),
+            "request_id".to_string(),
+            "app_id".to_string(),
+            1,
+            ApiProtocol::Bridge,
+            "method".to_string(),
+            Some("cid".to_string()),
+            true,
+        );
+        let rpc_request = RpcRequest::new("new_method".to_string(), "params".to_string(), ctx);
+        let data = JsonRpcApiResponse::mock();
+        let mut output: BrokerOutput = BrokerOutput { data: data.clone() };
+        let filter = "if .result and .result.success then (.result.interfaces | .[] | select(.connected) | {\"state\": \"connected\",\"type\": .interface | ascii_downcase }) elif .error then if .error.code == -32601 then \"Unknown method.\" else \"Error occurred with a different code\" end else \"No result or recognizable error\" end".to_string();
+        let mut response = JsonRpcApiResponse::mock();
+        response.result = Some(result);
+        apply_response(response, filter, &rpc_request, &mut output);
+        assert_eq!(output.data.result.unwrap(), json!({"state":"connected", "type":"wifi"}));
     }
 }
