faafffbbe34d6464150a8cef19f826230774781d
maggie98choy <maggie98choy@yahoo.com>
2024-10-10 09:48:26 -0700

    validating fastwebsockets lib for improving performance

--- core/main/src/firebolt/firebolt_ws.rs
+++ core/main/src/firebolt/firebolt_ws.rs
@@ -15,7 +15,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr};
 
 use super::firebolt_gateway::FireboltGatewayCommand;
 use crate::{
@@ -35,14 +35,19 @@ use ripple_sdk::{
         net::{TcpListener, TcpStream},
         sync::{mpsc, oneshot},
     },
-    utils::channel_utils::oneshot_send_and_log,
+    utils::{channel_utils::oneshot_send_and_log, error::RippleError},
     uuid::Uuid,
 };
 use ripple_sdk::{log::debug, tokio};
-use tokio_tungstenite::{
-    tungstenite::{self, Message},
-    WebSocketStream,
-};
+use fastwebsockets::{upgrade, FragmentCollectorRead};
+use fastwebsockets::WebSocketError;
+use hyper::service::service_fn;
+use hyper::Request;
+use hyper::Response;
+use http_body_util::Empty;
+use hyper::body::Bytes;
+use hyper::body::Incoming;
+use hyper::server::conn::http1;
 #[allow(dead_code)]
 pub struct FireboltWs {}
 
@@ -65,77 +70,6 @@ pub struct ConnectionCallback(ConnectionCallbackConfig);
  * Gets a query parameter from the request at the given key.
  * If required=true, then return an error if the param is missing
  */
-fn get_query(
-    req: &tungstenite::handshake::server::Request,
-    key: &'static str,
-    required: bool,
-) -> Result<Option<String>, tungstenite::handshake::server::ErrorResponse> {
-    let found_q = match req.uri().query() {
-        Some(qs) => {
-            let qs = querystring::querify(qs);
-            qs.iter().find(|q| q.0 == key).cloned()
-        }
-        None => None,
-    };
-
-    if required && found_q.is_none() {
-        let err_msg = format!("{} query parameter missing", key);
-        error!("{}", err_msg);
-        let err = tungstenite::http::response::Builder::new()
-            .status(403)
-            .body(Some(err_msg))
-            .unwrap();
-        return Err(err);
-    }
-    Ok(found_q.map(|q| String::from(q.1)))
-}
-
-impl tungstenite::handshake::server::Callback for ConnectionCallback {
-    fn on_request(
-        self,
-        request: &tungstenite::handshake::server::Request,
-        response: tungstenite::handshake::server::Response,
-    ) -> Result<
-        tungstenite::handshake::server::Response,
-        tungstenite::handshake::server::ErrorResponse,
-    > {
-        info!("New firebolt connection {:?}", request.uri().query());
-        let cfg = self.0;
-        let app_id_opt = match cfg.secure {
-            true => None,
-            false => match get_query(request, "appId", false)? {
-                Some(a) => Some(a),
-                None => cfg.internal_app_id,
-            },
-        };
-        let session_id = match app_id_opt {
-            Some(_) => Uuid::new_v4().to_string(),
-            // can unwrap here because if session is not given, then error will be returned
-            None => get_query(request, "session", true)?.unwrap(),
-        };
-        let app_id = match app_id_opt {
-            Some(a) => a,
-            None => {
-                if let Some(app_id) = cfg.app_state.get_app_id_from_session_id(&session_id) {
-                    app_id
-                } else {
-                    let err = tungstenite::http::response::Builder::new()
-                        .status(403)
-                        .body(Some(format!(
-                            "No application session found for {}",
-                            session_id
-                        )))
-                        .unwrap();
-                    error!("No application session found for app_id={}", session_id);
-                    return Err(err);
-                }
-            }
-        };
-        let cid = ClientIdentity { session_id, app_id };
-        oneshot_send_and_log(cfg.next, cid, "ResolveClientIdentity");
-        Ok(response)
-    }
-}
 
 impl FireboltWs {
     pub async fn start(
@@ -152,58 +86,55 @@ impl FireboltWs {
         let app_state = state.app_manager_state.clone();
         // Let's spawn the handling of each connection in a separate task.
         while let Ok((stream, client_addr)) = listener.accept().await {
-            let (connect_tx, connect_rx) = oneshot::channel::<ClientIdentity>();
-            let cfg = ConnectionCallbackConfig {
-                next: connect_tx,
-                app_state: app_state.clone(),
-                secure,
-                internal_app_id: internal_app_id.clone(),
-            };
-            match tokio_tungstenite::accept_hdr_async(stream, ConnectionCallback(cfg)).await {
-                Err(e) => {
-                    error!("websocket connection error {:?}", e);
-                }
-                Ok(ws_stream) => {
-                    trace!("websocket connection success");
-                    let state_for_connection_c = state_for_connection.clone();
-                    tokio::spawn(async move {
-                        FireboltWs::handle_connection(
-                            client_addr,
-                            ws_stream,
-                            connect_rx,
-                            state_for_connection_c.clone(),
-                            secure,
-                        )
-                        .await;
-                    });
-                }
-            }
+            let state_for_connection_c = state_for_connection.clone();
+            let internal_app_id_c = internal_app_id.clone();
+            tokio::spawn(async move {
+                let io = hyper_util::rt::TokioIo::new(stream);
+                // let conn_fut = Http::new().serve_connection(io, service_fn(Self::server_upgrade));
+                let conn_fut = http1::Builder::new()
+                    .serve_connection(io, service_fn(move |req| Self::handle_connection(req, state_for_connection_c.clone(), secure.clone(), internal_app_id_c.clone()))).with_upgrades();
+              if let Err(e) = conn_fut.await {
+                println!("An error occurred: {:?}", e);
+              }
+            });
         }
     }
 
     async fn handle_connection(
-        _client_addr: SocketAddr,
-        ws_stream: WebSocketStream<TcpStream>,
-        connect_rx: oneshot::Receiver<ClientIdentity>,
+        mut req: Request<Incoming>,
         state: PlatformState,
         gateway_secure: bool,
-    ) {
-        let identity = connect_rx.await.unwrap();
+        internal_app_id: Option<String>
+      ) -> Result<Response<Empty<Bytes>>, WebSocketError> {
+        let (response, fut) = upgrade::upgrade(&mut req)?;
+        let uri = req.uri();
+        let state_for_connection = state.clone();
         let client = state.get_client();
-        let app_id = identity.app_id.clone();
+
+        // start register connection session
         let (session_tx, mut resp_rx) = mpsc::channel(32);
+        let query_segs: Vec<&str> = uri.query().unwrap().split("&").collect::<Vec<&str>>();
+        let query_map: HashMap<String, String> = query_segs
+            .iter()
+            .map(|seg| {
+                let kv: Vec<&str> = seg.split("=").collect::<Vec<&str>>();
+                (kv[0].to_string(), kv[1].to_string())
+            })
+            .collect();
+
+        // let app_id = tags.get("app_id").unwrap_or(&"".to_string()).to_string();
         let ctx = ClientContext {
-            session_id: identity.session_id.clone(),
-            app_id: app_id.clone(),
-            gateway_secure,
+            session_id: query_map.get("sessionId").unwrap_or(&(Uuid::new_v4().to_string()).to_string()).to_string(),
+            app_id: query_map.get("appId").unwrap_or(&(Uuid::new_v4().to_string()).to_string()).to_string(),
+            gateway_secure: gateway_secure
         };
         let session = Session::new(
-            identity.app_id.clone(),
+            ctx.app_id.clone(),
             Some(session_tx.clone()),
             ripple_sdk::api::apps::EffectiveTransport::Websocket,
         );
-        let app_id_c = app_id.clone();
-        let session_id_c = identity.session_id.clone();
+        let app_id_c = ctx.app_id.clone();
+        let session_id_c = ctx.session_id.clone();
 
         let connection_id = Uuid::new_v4().to_string();
         info!(
@@ -216,95 +147,115 @@ impl FireboltWs {
             session_id: connection_id.clone(),
             session,
         };
+        println!("^^^ in fireboltWs handle_connection registering connection");
+
         if let Err(e) = client.send_gateway_command(msg) {
             error!("Error registering the connection {:?}", e);
-            return;
+            return Err(WebSocketError::ConnectionClosed);
         }
         if !gateway_secure
-            && PermissionHandler::fetch_and_store(&state, &app_id, false)
+            && PermissionHandler::fetch_and_store(&state, &app_id_c, false)
                 .await
                 .is_err()
         {
             error!("Couldnt pre cache permissions");
         }
+        //end of register connection session
 
-        let (mut sender, mut receiver) = ws_stream.split();
-        tokio::spawn(async move {
-            while let Some(rs) = resp_rx.recv().await {
-                let send_result = sender.send(Message::Text(rs.jsonrpc_msg.clone())).await;
-                match send_result {
-                    Ok(_) => {
-                        debug!(
-                            "Sent Firebolt response cid={} msg={}",
-                            connection_id_c, rs.jsonrpc_msg
-                        );
-                    }
-                    Err(err) => error!("{:?}", err),
-                }
-            }
-            debug!(
-                "api msg rx closed {} {} {}",
-                app_id_c, session_id_c, connection_id_c
-            );
+
+
+        tokio::task::spawn(async move {
+          if let Err(e) = tokio::task::unconstrained(Self::handle_request(fut, state_for_connection, gateway_secure, resp_rx, ctx, connection_id_c)).await {
+            eprintln!("Error in websocket connection: {}", e);
+          }
         });
-        while let Some(msg) = receiver.next().await {
-            match msg {
-                Ok(msg) => {
-                    if msg.is_text() && !msg.is_empty() {
-                        let req_text = String::from(msg.to_text().unwrap());
-                        let req_id = Uuid::new_v4().to_string();
-                        if let Ok(request) = RpcRequest::parse(
-                            req_text.clone(),
-                            ctx.app_id.clone(),
-                            ctx.session_id.clone(),
-                            req_id.clone(),
-                            Some(connection_id.clone()),
-                            ctx.gateway_secure,
-                        ) {
-                            debug!(
-                                "firebolt_ws Received Firebolt request {} {} {}",
-                                connection_id, request.ctx.request_id, request.method
-                            );
-                            let msg = FireboltGatewayCommand::HandleRpc { request };
-                            if let Err(e) = client.clone().send_gateway_command(msg) {
-                                error!("failed to send request {:?}", e);
-                            }
-                        } else {
-                            if let Some(session) = &state
-                                .session_state
-                                .get_session_for_connection_id(&connection_id)
-                            {
-                                use ripple_sdk::api::apps::EffectiveTransport;
-                                let err =
-                                    ErrorResponse::new(ErrorCode::InvalidRequest.into(), Id::Null);
-                                let msg = serde_json::to_string(&err).unwrap();
-                                let api_msg =
-                                    ApiMessage::new(ApiProtocol::JsonRpc, msg, req_id.clone());
-                                match session.get_transport() {
-                                    EffectiveTransport::Bridge(id) => {
-                                        let _ = state.send_to_bridge(id, api_msg).await;
-                                    }
-                                    EffectiveTransport::Websocket => {
-                                        let _ = session.send_json_rpc(api_msg).await;
-                                    }
+      
+        Ok(response)
+      }
+
+    async fn handle_request(fut: upgrade::UpgradeFut, state: PlatformState, secure: bool, mut resp_rx: mpsc::Receiver<ApiMessage>, ctx: ClientContext, connection_id: String) -> Result<(), WebSocketError> {
+    // let mut ws = fastwebsockets::FragmentCollector::new(fut.await?);
+    let ws = fut.await?;
+    let (sender, mut receiver) = ws.split(tokio::io::split);
+    let mut sender = FragmentCollectorRead::new(sender);
+
+    tokio::spawn(async move {
+        while let Some(rs) = resp_rx.recv().await {
+            let send_result = receiver.write_frame(fastwebsockets::Frame::text(rs.jsonrpc_msg.into_bytes().to_vec().into())).await;
+            match send_result {
+                Ok(_) => {
+                    debug!(
+                        "Sent Firebolt response",
+                    );
+                }
+                Err(err) => error!("{:?}", err),
+            }
+        }
+        // debug!(
+        //     "api msg rx closed {} {} {}",
+        //     app_id_c, session_id_c, connection_id_c
+        // );
+    });
+
+    loop {
+        let frame = sender.read_frame::<_, WebSocketError>(&mut move |_| async {
+                unreachable!();
+            })
+            .await?;  
+        let msg = String::from_utf8(frame.payload.to_vec());
+        match msg {
+            Ok(msg) => {
+                if !msg.is_empty() {
+                    //let req_text = String::from(msg.to_text().unwrap());
+                    let req_id = Uuid::new_v4().to_string();
+                    if let Ok(request) = RpcRequest::parse(
+                        msg.clone(),
+                        ctx.app_id.clone(),
+                        ctx.session_id.clone(),
+                        req_id.clone(),
+                        Some(connection_id.clone()),
+                        ctx.gateway_secure,
+                    ) {
+                        debug!(
+                            "firebolt_ws Received Firebolt request {} {} {}",
+                            connection_id, request.ctx.request_id, request.method
+                        );
+                        let msg = FireboltGatewayCommand::HandleRpc { request };
+                        let client = state.get_client();
+                        let res = client.clone().send_gateway_command(msg);
+                        if let Err(e) = res {
+                            error!("failed to send request {:?}", e);
+                        }
+                    } else {
+                        if let Some(session) = &state
+                            .session_state
+                            .get_session_for_connection_id(&connection_id)
+                        {
+                            use ripple_sdk::api::apps::EffectiveTransport;
+                            let err =
+                                ErrorResponse::new(ErrorCode::InvalidRequest.into(), Id::Null);
+                            let msg = serde_json::to_string(&err).unwrap();
+                            let api_msg =
+                                ApiMessage::new(ApiProtocol::JsonRpc, msg, req_id.clone());
+                            match session.get_transport() {
+                                EffectiveTransport::Bridge(id) => {
+                                    let _ = state.send_to_bridge(id, api_msg).await;
+                                }
+                                EffectiveTransport::Websocket => {
+                                    let _ = session.send_json_rpc(api_msg).await;
                                 }
                             }
-                            error!("invalid message {}", req_text)
                         }
+                        // error!("invalid message {}", req_text)
                     }
                 }
-                Err(e) => {
-                    error!("ws error cid={} error={:?}", connection_id, e);
-                }
+            }
+            
+            Err(e) => {
+                // error!("ws error cid={} error={:?}", connection_id, e);
             }
         }
-        debug!("SESSION DEBUG Unregistering {}", connection_id);
-        let msg = FireboltGatewayCommand::UnregisterSession {
-            session_id: identity.session_id.clone(),
-            cid: connection_id,
-        };
-        if let Err(e) = client.send_gateway_command(msg) {
-            error!("Error Unregistering {:?}", e);
-        }
+    }    
+    Ok(())
     }
 }
