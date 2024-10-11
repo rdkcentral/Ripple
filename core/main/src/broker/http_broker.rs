eef8d25d42fa8f949644e50a2a2609867059b203
maggie98choy <maggie98choy@yahoo.com>
2024-10-11 12:43:05 -0700

    validate fastwebsocket lib

--- core/main/src/broker/http_broker.rs
+++ core/main/src/broker/http_broker.rs
@@ -17,7 +17,10 @@
 
 use std::vec;
 
-use hyper::{client::HttpConnector, Body, Client, Method, Request, Response, Uri};
+use http_body_util::StreamBody;
+// use hyper::{client::HttpConnector, Body, Client, Method, Request, Response};
+use reqwest::{Body, Client, Method, Request, Response};
+// use http::{Method, Request, Uri};
 use ripple_sdk::{
     log::{debug, error, trace},
     tokio::{self, sync::mpsc},
@@ -40,18 +43,18 @@ pub struct HttpBroker {
 */
 
 async fn send_http_request(
-    client: &Client<HttpConnector>,
+    client: &Client,
     method: Method,
-    uri: &Uri,
+    uri: String,
     path: &str,
-) -> Result<Response<Body>, RippleError> {
+) -> Result<Response, RippleError> {
     /*
     TODO? we may need to support body for POST request in the future
     */
-    let http_request = Request::new(Body::empty());
-    let (mut parts, _) = http_request.into_parts();
-    //TODO, need to refactor to support other methods
-    parts.method = method.clone();
+    // let http_request = Request::new(Body::empty());
+    // let (mut parts, _) = http_request.into_parts();
+    // //TODO, need to refactor to support other methods
+    // parts.method = method.clone();
     /*
     mix endpoint url with method
     */
@@ -59,25 +62,34 @@ async fn send_http_request(
     TODONT: unwraps are bad, need to handle errors
     */
 
-    let uri: Uri = format!("{}{}", uri, path)
-        .parse()
-        .map_err(|e: InvalidUri| RippleError::BrokerError(e.to_string()))?;
-    let new_request = Request::builder()
-        .uri(uri)
-        .body(Body::empty())
-        .map_err(|e| RippleError::BrokerError(e.to_string()))?;
-    let (uri_parts, _) = new_request.into_parts();
+    // let uri: Uri = format!("{}{}", uri, path)
+    //     .parse()
+    //     .map_err(|e: InvalidUri| RippleError::BrokerError(e.to_string()))?;
+    let url = format!("{}{}", uri, path);
+    let response = client.post(url).body("").send().await;
+    // let new_request = Request::builder()
+    //     .uri(uri)
+    //     .body(Body::empty())
+    //     .map_err(|e| RippleError::BrokerError(e.to_string()))?;
+    // let (uri_parts, _) = new_request.into_parts();
 
-    parts.uri = uri_parts.uri;
+    // parts.uri = uri_parts.uri;
 
-    let http_request = Request::from_parts(parts, Body::empty());
+    // let http_request = Request::from_parts(parts, Body::empty());
 
-    debug!(
-        "http_broker sending {} request={}",
-        method,
-        http_request.uri(),
-    );
-    match client.request(http_request).await {
+    // debug!(
+    //     "http_broker sending {} request={}",
+    //     method,
+    //     http_request.uri(),
+    // );
+    // match client.request(http_request).await {
+    //     Ok(v) => Ok(v),
+    //     Err(e) => {
+    //         error!("Error in server");
+    //         Err(RippleError::BrokerError(e.to_string()))
+    //     }
+    // }
+    match response {
         Ok(v) => Ok(v),
         Err(e) => {
             error!("Error in server");
@@ -102,18 +114,18 @@ fn error_string_to_json(msg: &str) -> serde_json::Value {
         "error": msg
     })
 }
-async fn body_to_bytes(body: Body) -> Vec<u8> {
-    match hyper::body::to_bytes(body).await {
-        Ok(bytes) => {
-            let value: Vec<u8> = bytes.into();
-            value.as_slice().to_vec()
-        }
-        Err(e) => format!("error in http broker transforming body to bytes {}", e)
-            .to_string()
-            .as_bytes()
-            .to_vec(),
-    }
-}
+// async fn body_to_bytes(body: Body) -> Vec<u8> {
+//     match hyper::body::to_bytes(body).await {
+//         Ok(bytes) => {
+//             let value: Vec<u8> = bytes.into();
+//             value.as_slice().to_vec()
+//         }
+//         Err(e) => format!("error in http broker transforming body to bytes {}", e)
+//             .to_string()
+//             .as_bytes()
+//             .to_vec(),
+//     }
+// }
 
 impl EndpointBroker for HttpBroker {
     fn get_broker(request: BrokerConnectRequest, callback: BrokerCallback) -> Self {
@@ -121,47 +133,73 @@ impl EndpointBroker for HttpBroker {
         let (tx, mut tr) = mpsc::channel(10);
         let broker = BrokerSender { sender: tx };
         let client = Client::new();
-        let _ =  endpoint.get_url().parse().map_err(|e| error!("broker url {:?} in endpoint is invalid, cannot start http broker. error={}",endpoint,e) ).map(|uri| tokio::spawn(async move {
+        // let _ =  endpoint.get_url().parse().map_err(|e| error!("broker url {:?} in endpoint is invalid, cannot start http broker. error={}",endpoint,e) ).map(|uri| tokio::spawn(async move {
+        let url = endpoint.get_url();
+        tokio::spawn(async move {
             while let Some(request) = tr.recv().await {
                 debug!("http broker received request={:?}", request);
-                match send_http_request(&client, Method::GET, &uri, &request.clone().rule.alias)
-                    .await
+                match send_http_request(
+                    &client,
+                    Method::GET,
+                    url.clone(),
+                    &request.clone().rule.alias,
+                )
+                .await
                 {
                     Ok(response) => {
-                        let (parts, body) = response.into_parts();
-                        let body = body_to_bytes(body).await;
+                        // let (parts, body) = response.into_parts();
+
+                        // let body = body_to_bytes(body).await;
+                        let parts = response.status();
+                        let body = response.bytes().await.unwrap().to_vec();
+
                         let mut request = request;
-                        if let Ok(json_str) = serde_json::from_slice::<serde_json::Value>(&body).map(|v| vec![v])
+                        if let Ok(json_str) = serde_json::from_slice::<serde_json::Value>(&body)
+                            .map(|v| vec![v])
                             .and_then(|v| serde_json::to_string(&v))
                         {
+                            println!("^ in http broker send http request");
                             request.rpc.params_json = json_str;
                             let response = Self::update_request(&request);
                             trace!(
                                 "http broker response={:?} to request: {:?} using rule={:?}",
-                                response, request, request.rule
+                                response,
+                                request,
+                                request.rule
                             );
 
                             send_broker_response(&callback, &request, &body).await;
-                            if !parts.status.is_success() {
+                            if !parts.is_success() {
                                 error!(
                                     "http error {} returned from http service in http broker {:?}",
-                                    parts.status, body
+                                    parts.as_str(),
+                                    body
                                 );
                             }
                         } else {
-                            let msg = format!("Error in http broker parsing response from http service at {}. status={:?}",uri, parts.status);
-                            error!("{}",msg);
-                            send_broker_response(&callback, &request,  error_string_to_json(msg.as_str()).to_string().as_bytes()).await;
+                            let msg = format!("Error in http broker parsing response from http service at {}. status={:?}",url, parts);
+                            error!("{}", msg);
+                            send_broker_response(
+                                &callback,
+                                &request,
+                                error_string_to_json(msg.as_str()).to_string().as_bytes(),
+                            )
+                            .await;
                         }
                     }
                     Err(err) => {
-                        let msg = format!("An error message from calling the downstream http service={} in http broker {:?}", uri, err);
-                        error!("{}",msg);
-                        send_broker_response(&callback, &request,  error_string_to_json(msg.as_str()).to_string().as_bytes()).await;
+                        let msg = format!("An error message from calling the downstream http service={} in http broker {:?}", url, err);
+                        error!("{}", msg);
+                        send_broker_response(
+                            &callback,
+                            &request,
+                            error_string_to_json(msg.as_str()).to_string().as_bytes(),
+                        )
+                        .await;
                     }
                 }
             }
-        }));
+        });
 
         Self {
             sender: broker,
