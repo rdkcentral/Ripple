9bb814623e2f14529a8524665f166f838068d686
pahearn73 <pahearn73@yahoo.com>
2024-10-14 10:27:37 -0400

    feat: RPPL-2624: Metrics initialization analysis: Clippy

--- core/main/src/bootstrap/extn/load_session_step.rs
+++ core/main/src/bootstrap/extn/load_session_step.rs
@@ -15,8 +15,6 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use std::time::{SystemTime, UNIX_EPOCH};
-
 use ripple_sdk::framework::bootstrap::Bootstep;
 use ripple_sdk::tokio;
 use ripple_sdk::{async_trait::async_trait, framework::RippleResponse};
