b5f3f38b9616c0810927a399384cc8431a63f7f3
Maggie Choy <maggie98choy@yahoo.com>
2024-12-11 22:48:53 -0800

    fix clippy error

--- core/sdk/src/api/manifest/device_manifest.rs
+++ core/sdk/src/api/manifest/device_manifest.rs
@@ -369,7 +369,7 @@ fn country_postal_code_default() -> HashMap<String, String> {
 }
 
 pub fn data_governance_default() -> DataGovernanceConfig {
-    DataGovernanceConfig::default()
+    DataGovernanceConfig { policies: vec![] }
 }
 
 #[derive(Deserialize, Debug, Clone, Default)]
@@ -564,14 +564,6 @@ impl DataGovernanceConfig {
     }
 }
 
-impl Default for DataGovernanceConfig {
-    fn default() -> Self {
-        DataGovernanceConfig {
-            policies: vec![],
-        }
-    }
-}
-
 impl Default for RippleFeatures {
     fn default() -> Self {
         RippleFeatures {
@@ -672,7 +664,7 @@ impl Default for RippleConfiguration {
             features: Default::default(),
             internal_app_id: None,
             saved_dir: default_saved_dir(),
-            data_governance: Default::default(),
+            data_governance: data_governance_default(),
             partner_exclusion_refresh_timeout: partner_exclusion_refresh_timeout_default(),
             metrics_logging_percentage: metrics_logging_percentage_default(),
             internet_monitoring_configuration: Default::default(),
