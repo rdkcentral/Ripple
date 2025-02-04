f3bd74e53ada9085e7a1bec5c83353670faaca7c
brendanobra <brendan@obrafamily.org>
2025-02-04 08:00:29 -0800

    chores: testing

--- coverage.sh
+++ coverage.sh
@@ -5,6 +5,7 @@ if [ -z "$INSTALLED" ]; then
     cargo install cargo-llvm-cov
 fi
 
+
 cargo llvm-cov --cobertura --output-path coverage.cobertura.xml --ignore-filename-regex ".*[\\/](.*distributor*)[\\/]?.*"
 CURRENT_COVERAGE=$(grep '<coverage' coverage.cobertura.xml | grep -o 'line-rate="[0-9.]\+"' | grep -o '[0-9.]\+')
 echo "Current coverage: $CURRENT_COVERAGE"
