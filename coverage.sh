7a999682db2fb09ffee1e56c8f8b169403782bab
brendanobra <brendan@obrafamily.org>
2025-02-04 08:06:26 -0800

    chore: updating coverage to match official(tm) coverage branch

--- coverage.sh
+++ coverage.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+#
+# This script is very based on .github/workflows/Build.yml
 INSTALLED=$(cargo install --list | grep cargo-llvm-cov)
 if [ -z "$INSTALLED" ]; then
     echo "Installing cargo-llvm-cov"
     cargo install cargo-llvm-cov
 fi
-
+LOWER_COVERAGE_THRESHOLD=$(cat ./ci/coverage_threshold.txt |  cut -d ' ' -f1)
 
 cargo llvm-cov --cobertura --output-path coverage.cobertura.xml --ignore-filename-regex ".*[\\/](.*distributor*)[\\/]?.*"
 CURRENT_COVERAGE=$(grep '<coverage' coverage.cobertura.xml | grep -o 'line-rate="[0-9.]\+"' | grep -o '[0-9.]\+')
-echo "Current coverage: $CURRENT_COVERAGE"
+CURRENT_COVERAGE=$(printf %.0f $(echo "$CURRENT_COVERAGE*100" | bc))
+
+echo "Current coverage: $CURRENT_COVERAGE, allowed coverage: $LOWER_COVERAGE_THRESHOLD"
