7c5f7df8269a0eecc0c6126a361f9681d051de0d
brendanobra <brendan@obrafamily.org>
2025-02-06 09:54:20 -0800

    feedback: truncate

--- core/main/src/broker/thunder/user_data_migrator.rs
+++ core/main/src/broker/thunder/user_data_migrator.rs
@@ -903,7 +903,7 @@ impl UserDataMigrator {
         // open the status file if exists, else create a new file
         let file = OpenOptions::new()
             .create(true)
-            .truncate(true)
+            .truncate(false)
             .write(true)
             .read(true)
             .open(status_file_path)
@@ -918,7 +918,7 @@ impl UserDataMigrator {
 
         let file = OpenOptions::new()
             .write(true)
-            .truncate(true)
+            .truncate(false)
             .create(true)
             .open(&self.status_file_path)
             .unwrap();
