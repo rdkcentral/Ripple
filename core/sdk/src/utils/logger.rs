d11c213dfacb5aa43857417ba087f9cf50eefb06
pkumbh631 <punam_kumbhar@comcast.com>
2025-03-22 10:24:29 -0400

    update logger.rs

--- core/sdk/src/utils/logger.rs
+++ core/sdk/src/utils/logger.rs
@@ -16,19 +16,18 @@
 //
 
 use log::LevelFilter;
-use std::{str::FromStr, sync::atomic::AtomicU32};
+use std::{str::FromStr, sync::atomic::AtomicU32, sync::Mutex};
 
 pub static LOG_COUNTER: AtomicU32 = AtomicU32::new(1);
-static mut LOG_SIGNAL_FILTER: LevelFilter = LevelFilter::Info;
+pub static LOG_SIGNAL_FILTER: Mutex<LevelFilter> = Mutex::new(LevelFilter::Info);
 
 pub fn set_log_signal_filter(filter: LevelFilter) {
-    unsafe {
-        LOG_SIGNAL_FILTER = filter;
-    }
+    let mut log_filter = LOG_SIGNAL_FILTER.lock().unwrap();
+    *log_filter = filter;
 }
 
 pub fn init_logger(name: String) -> Result<(), fern::InitError> {
-    let log_string: String = std::env::var("RUST_LOG").unwrap_or_else(|_| "debug".into());
+    let log_string: String = std::env::var("RUST_LOG").unwrap_or_else(|_| "INFO".into());
     println!("log level {}", log_string);
     let filter = log::LevelFilter::from_str(&log_string).unwrap_or(log::LevelFilter::Info);
     fern::Dispatch::new()
@@ -52,7 +51,7 @@ pub fn init_logger(name: String) -> Result<(), fern::InitError> {
             ));
         })
         .level(filter)
-        .level_for("log_signal", unsafe { LOG_SIGNAL_FILTER })
+        .level_for("log_signal", *LOG_SIGNAL_FILTER.lock().unwrap())
         //log filter applied here, making the log level to OFF for the below mentioned crates
         .level_for("h2", log::LevelFilter::Off)
         .level_for("hyper", log::LevelFilter::Off)
@@ -67,7 +66,7 @@ pub fn init_logger(name: String) -> Result<(), fern::InitError> {
 }
 
 pub fn init_and_configure_logger(version: &str, name: String) -> Result<(), fern::InitError> {
-    let log_string: String = std::env::var("RUST_LOG").unwrap_or_else(|_| "debug".into());
+    let log_string: String = std::env::var("RUST_LOG").unwrap_or_else(|_| "INFO".into());
     println!("log level {}", log_string);
     let _version_string = version.to_string();
     let filter = log::LevelFilter::from_str(&log_string).unwrap_or(log::LevelFilter::Info);
@@ -118,7 +117,7 @@ pub fn init_and_configure_logger(version: &str, name: String) -> Result<(), fern
             }
         })
         .level(filter)
-        .level_for("log_signal", unsafe { LOG_SIGNAL_FILTER })
+        .level_for("log_signal", *LOG_SIGNAL_FILTER.lock().unwrap())
         //log filter applied here, making the log level to OFF for the below mentioned crates
         .level_for("h2", log::LevelFilter::Off)
         .level_for("hyper", log::LevelFilter::Off)
