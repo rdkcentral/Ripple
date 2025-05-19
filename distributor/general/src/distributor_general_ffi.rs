763dfceff1f423f73e48a3e25c6816642e3c610a
kvfasil <fasilkvclt@gmail.com>
2025-05-19 12:42:04 -0400

    removed general distributor SecureStorage Processor.

--- distributor/general/src/distributor_general_ffi.rs
+++ distributor/general/src/distributor_general_ffi.rs
@@ -38,8 +38,7 @@ use ripple_sdk::{
 
 use crate::{
     general_permission_processor::DistributorPermissionProcessor,
-    general_privacy_processor::DistributorPrivacyProcessor,
-    general_securestorage_processor::DistributorSecureStorageProcessor,
+    general_privacy_processor::DistributorPrivacyProcessor
 };
 
 fn init_library() -> CExtnMetadata {
@@ -83,8 +82,6 @@ fn start_launcher(sender: ExtnSender, receiver: CReceiver<CExtnMessage>) {
             }
 
             client.add_request_processor(DistributorPermissionProcessor::new(client.clone()));
-            client.add_request_processor(DistributorSecureStorageProcessor::new(client.clone()));
-
             // Lets Main know that the distributor channel is ready
             let _ = client.event(ExtnStatus::Ready);
         });
