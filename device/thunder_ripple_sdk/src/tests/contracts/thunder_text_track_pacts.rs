3ab6fac858628c0c00ea1c70af972fe1358c2c90
satlead <satlead@gmail.com>
2025-05-22 07:10:27 -0400

    fix: clippy dependencies

--- device/thunder_ripple_sdk/src/tests/contracts/thunder_text_track_pacts.rs
+++ device/thunder_ripple_sdk/src/tests/contracts/thunder_text_track_pacts.rs
@@ -37,9 +37,9 @@ use futures_util::{SinkExt, StreamExt};
 #[allow(dead_code, unused_imports)]
 use tokio::time::{timeout, Duration};
 #[allow(dead_code, unused_imports)]
-use tokio_tungstenite::connect_async;
+use ripple_sdk::tokio_tungstenite::connect_async;
 #[allow(dead_code, unused_imports)]
-use tokio_tungstenite::tungstenite::protocol::Message;
+use ripple_sdk::tokio_tungstenite::tungstenite::protocol::Message;
 
 #[tokio::test(flavor = "multi_thread")]
 #[cfg_attr(not(feature = "websocket_contract_tests"), ignore)]
