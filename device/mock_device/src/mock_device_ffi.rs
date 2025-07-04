3b91430a8842ffedcd864203564220d023c9e1e7
Maggie Choy <maggie98choy@yahoo.com>
2025-07-03 18:50:55 -0700

    enable mock device to function ad independent systemd service

--- device/mock_device/src/mock_device_ffi.rs
+++ device/mock_device/src/mock_device_ffi.rs
@@ -17,14 +17,14 @@
 
 use jsonrpsee::core::server::rpc_module::Methods;
 use ripple_sdk::{
-    api::manifest::ripple_manifest_loader::RippleManifestLoader,
+    api::{gateway::rpc_gateway_api::ApiMessage, manifest::ripple_manifest_loader::RippleManifestLoader},
     export_extn_channel,
     extn::{
         client::extn_client::ExtnClient,
         extn_id::{ExtnClassId, ExtnId},
         ffi::ffi_channel::ExtnChannel,
     },
-    log::error,
+    log::{error, debug},
     processor::rpc_request_processor::RPCRequestProcessor,
     tokio::{self, runtime::Runtime},
     utils::logger::init_and_configure_logger,
@@ -35,8 +35,71 @@ use crate::{
     mock_device_controller::{MockDeviceController, MockDeviceControllerServer},
     utils::boot_ws_server,
 };
+use tokio::sync::mpsc;
 
 pub const EXTN_NAME: &str = "mock_device";
+
+pub async fn start_service() {
+    let log_lev = ripple_sdk::log::LevelFilter::Debug;
+    let _ = init_and_configure_logger(
+        "some_version",
+        EXTN_NAME.into(),
+        Some(vec![
+            ("extn_manifest".to_string(), log_lev),
+            ("device_manifest".to_string(), log_lev),
+        ]),
+    );
+
+    let Ok((extn_manifest, _device_manifest)) = RippleManifestLoader::initialize() else {
+        error!("Error initializing manifests");
+        return;
+    };
+    let runtime = match Runtime::new() {
+        Ok(r) => r,
+        Err(err) => {
+            error!("Error creating runtime: {}", err);
+            return;
+        }
+    };
+
+    let id = ExtnId::new_channel(ExtnClassId::Device, EXTN_NAME.to_string()).to_string();
+    let symbol = extn_manifest.get_extn_symbol(&id);
+    if symbol.is_none() {
+        error!("Error getting symbol");
+        return;
+    }
+    let symbol = symbol.unwrap();
+    let (mut client, tr) = ExtnClient::new_extn(symbol);
+
+    init(client.clone(), tr).await;
+
+}
+
+async fn init(
+    mut client: ExtnClient,
+    tr: mpsc::Receiver<ApiMessage>
+) {
+        let client_c = client.clone();
+        tokio::spawn(async move {
+            match boot_ws_server(client.clone()).await {
+                Ok(server) => {
+                    let state = MockDeviceState::new(server);
+
+                    let mut methods = Methods::new();
+                    let _ = methods.merge(MockDeviceController::new(state).into_rpc());
+                    let processor = RPCRequestProcessor::new(
+                        client.clone(),
+                        methods,
+                        ExtnId::new_channel(ExtnClassId::Device, "mock_device".into()),
+                    );
+                    client.add_request_processor(processor);
+                }
+                Err(err) => panic!("websocket server failed to start. {}", err),
+            };
+        });
+
+        client_c.initialize(tr).await;
+}
 
 fn start() {
     let Ok((extn_manifest, _device_manifest)) = RippleManifestLoader::initialize() else {
@@ -60,26 +123,7 @@ fn start() {
     let symbol = symbol.unwrap();
     let (mut client, tr) = ExtnClient::new_extn(symbol);
     runtime.block_on(async move {
-        let client_c = client.clone();
-        tokio::spawn(async move {
-            match boot_ws_server(client.clone()).await {
-                Ok(server) => {
-                    let state = MockDeviceState::new(server);
-
-                    let mut methods = Methods::new();
-                    let _ = methods.merge(MockDeviceController::new(state).into_rpc());
-                    let processor = RPCRequestProcessor::new(
-                        client.clone(),
-                        methods,
-                        ExtnId::new_channel(ExtnClassId::Device, "mock_device".into()),
-                    );
-                    client.add_request_processor(processor);
-                }
-                Err(err) => panic!("websocket server failed to start. {}", err),
-            };
-        });
-
-        client_c.initialize(tr).await;
+        init(client.clone(), tr).await;
     });
 }
 
