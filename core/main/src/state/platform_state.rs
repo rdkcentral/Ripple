6ca075efcf140c1108f2b620d1481951ba41d245
kvfasil <fasilkvclt@gmail.com>
2025-07-17 11:35:30 -0400

    moving RuleEngine into arc shared reference.
    Optimized jq_compile funtion for memory usage.
    
    moving Manifest feild into Arc and thread safe.
    
    fixed fmt,clippy error.

--- core/main/src/state/platform_state.rs
+++ core/main/src/state/platform_state.rs
@@ -35,7 +35,7 @@ use ripple_sdk::{
     utils::error::RippleError,
     uuid::Uuid,
 };
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use crate::{
     broker::{endpoint_broker::EndpointBrokerState, rules::rules_engine::RuleEngine},
@@ -93,8 +93,8 @@ impl From<String> for DeviceSessionIdentifier {
 
 #[derive(Debug, Clone)]
 pub struct PlatformState {
-    pub extn_manifest: ExtnManifest,
-    device_manifest: DeviceManifest,
+    pub extn_manifest: Arc<ExtnManifest>,
+    device_manifest: Arc<DeviceManifest>,
     pub ripple_client: RippleClient,
     pub app_library_state: AppLibraryState,
     pub session_state: SessionState,
@@ -127,15 +127,15 @@ impl PlatformState {
         let provider_registations = extn_manifest.provider_registrations.clone();
         let metrics_state = OpMetricState::default();
         Self {
-            extn_manifest,
+            extn_manifest: Arc::new(extn_manifest),
             cap_state: CapState::new(manifest.clone()),
             session_state: SessionState::default(),
-            device_manifest: manifest.clone(),
+            device_manifest: Arc::new(manifest.clone()),
             ripple_client: client.clone(),
             app_library_state: AppLibraryState::new(app_library),
             app_events_state: AppEventsState::default(),
             provider_broker_state: ProviderBrokerState::default(),
-            app_manager_state: AppManagerState::new(&manifest.configuration.saved_dir),
+            app_manager_state: AppManagerState::new(&manifest.configuration.saved_dir.clone()),
             open_rpc_state: OpenRpcState::new(Some(exclusory), extn_sdks, provider_registations),
             router_state: RouterState::new(),
             metrics: metrics_state.clone(),
@@ -165,15 +165,15 @@ impl PlatformState {
     }
 
     pub fn get_manifest(&self) -> ExtnManifest {
-        self.extn_manifest.clone()
+        (*self.extn_manifest).clone()
     }
 
     pub fn get_rpc_aliases(&self) -> HashMap<String, Vec<String>> {
-        self.extn_manifest.clone().rpc_aliases
+        self.extn_manifest.rpc_aliases.clone()
     }
 
     pub fn get_device_manifest(&self) -> DeviceManifest {
-        self.device_manifest.clone()
+        (*self.device_manifest).clone()
     }
 
     pub fn get_client(&self) -> RippleClient {
