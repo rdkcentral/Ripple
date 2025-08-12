c4837351d5a762c1a3c575ba43751625328c07a4
kvfasil <fasilkvclt@gmail.com>
2025-08-12 11:13:57 -0400

    fix : code update.

--- core/sdk/src/api/manifest/cascaded_extn_manifest.rs
+++ core/sdk/src/api/manifest/cascaded_extn_manifest.rs
@@ -74,7 +74,7 @@ impl MergeConfig<CascadedExtnManifest> for ExtnManifest {
         }
         if let Some(cas_rules_path) = cascaded.rules_path {
             self.rules_path.extend(cas_rules_path);
-            self.extn_sdks.sort();
+            self.rules_path.sort();
         }
         if let Some(cas_extn_sdks) = cascaded.extn_sdks {
             self.extn_sdks.extend(cas_extn_sdks);
